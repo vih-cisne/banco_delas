bdae2ade9102e5dec634ac9031e54c643ca4454e
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-08-06 20:39:07 -0300

    feat: readme

--- banco_delas.py
+++ banco_delas.py
@@ -1,12 +1,6 @@
-# O banco Banco Delas é um banco moderno e eficiente, com vantagens exclusivas para clientes mulheres.
 
 from Cliente import Cliente
 from Conta_Corrente import Conta_Corrente
-
-
-
-# Modele um sistema orientado a objetos para representar contas correntes em do Banco Delas seguindo os requisitos abaixo.
-
                     
 
 def app_banco():
@@ -53,25 +47,3 @@ app_banco()
 
  
 
-# 1. Cada conta corrente pode ter um ou mais clientes como titular.
-
-# 2. O banco controla apenas o nome, o telefone e a renda mensal de cada cliente.
-
-# 3. A conta corrente apresenta um saldo e uma lista de operações de saques e depósitos.
-
-#    Quando a cliente fizer um saque, diminuiremos o saldo da conta corrente. Quando ela 
-
-#    fizer um depósito, aumentaremos o saldo.
-
-# 4. Clientes mulheres possuem em suas contas um cheque especial de valor igual à sua renda
-
-#    mensal, ou seja, elas podem sacar valores que deixam a sua conta com valor negativo até
-
-#    -renda_mensal.
-
-# 5. Clientes homens por enquanto não têm direito a cheque especial.
- 
-
-# Para modelar seu sistema, utilize obrigatoriamente os conceitos "classe", "herança" e "polimorfismo".
-
-# Opcionalmente, você pode também utilizar "propriedades", "encapsulamento" e "classe abstrata".
